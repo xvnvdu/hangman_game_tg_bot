85e98193834c1624154513edc8fcbbd9525b84f9
xvnvdu <59318052+xvnvdu@users.noreply.github.com>
2024-08-13 00:58:53 +0300

    Update bot.py

--- hangman_game_tg_bot/bot.py
+++ hangman_game_tg_bot/bot.py
@@ -5,9 +5,6 @@ from telebot import types
 
 bot = telebot.TeleBot('your_token')
 
-eng = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-ru = ['а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я']
-
 
 @bot.message_handler(commands=['start'])
 def start_bot(message):
